800de26a9c0f3911ab61a7db7ae7105a068a26ce
Alexey Koptyaev <agreen90@yandex.ru>
2017-09-06 22:10:09 +0300

    Add project files.

---
+++ StringFormatter/Formatter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace StringFormatter
+{
+    public class Formatter
+    {
+        private Dictionary<string, List<Func<string, string>>> _rules = new Dictionary<string, List<Func<string, string>>>();
+
+        public void AddRule(string propertyName, List<Func<string, string>> toRemove)
+        {
+            _rules[propertyName] = toRemove;
+        }
+
+        public void AddRule(string propertyName, Func<string, string> toRemove)
+        {
+            _rules[propertyName] = new List<Func<string, string>>() { toRemove };
+        }
+
+        public void RemoveRule(string propertyName)
+        {
+            _rules.Remove(propertyName);
+        }
+
+        public object Format(object obj)
+        {
+            var jObj = obj as JObject;
+            var newObj = new JObject();
+
+            foreach (var value in jObj.Values())
+            {
+                string temp = value.ToString();
+
+                List<Func<string, string>> funcList;
+
+                if (_rules.TryGetValue(value.Path, out funcList))
+                {
+                    foreach (var func in _rules[value.Path])
+                    {
+                        temp = func(temp);
+                    }
+                }
+
+                newObj.Add(value.Path, temp);
+            }
+
+            return newObj;
+        }
+    }
+}
---
+++ StringFormatter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new JObject();
+            obj.Add("message", "asd:fdsfgs:Fault--[[Message:ImessageCreated");
+            obj.Add("headers", "someOtherStuff");
+
+
+            var formatter = new Formatter();
+            formatter.AddRule("message", (x) => x.Replace("--", ""));
+
+            var q = formatter.Format(obj);
+        }
+    }
+}
